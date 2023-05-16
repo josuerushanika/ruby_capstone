197703e60074acc25e218538c2570a229d70d1a2
josuerushanika <josuerushanika@gmail.com>
2023-05-16 14:30:33 +0200

    Fix linters

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,14 +1,14 @@
 require_relative './item'
 
 class MusicAlbum < Item
-    attr_reader :on_spotify
+  attr_reader :on_spotify
 
-    def initialize(on_spotify)
-        super()
-        @on_spotify = on_spotify
-    end
+  def initialize(on_spotify)
+    super()
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        @on_spotify == true && super
-    end
+  def can_be_archived?
+    @on_spotify == true && super
+  end
 end
