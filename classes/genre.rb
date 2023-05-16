e7d1373ae28607fc3dba2deaa589a74b063f1504
josuerushanika <josuerushanika@gmail.com>
2023-05-16 13:58:09 +0200

    Add add_item mthd in genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,15 @@
 class Genre
-    
+    attr_reader :id
+    attr_accessor :name, :items
+
+    def initialize(name)
+        @id = Random.rand(1..1000)
+        @name = name
+        @items = []
+    end
+
+    def add_item(item)
+        item.genre = self
+        @item << item unless @items.include?(item)
+    end
 end
