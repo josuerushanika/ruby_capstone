ae25fd561ad668e9b3ac6a9758e639f10078b736
josuerushanika <josuerushanika@gmail.com>
2023-05-17 18:55:03 +0200

    Upadte test

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,21 +1,15 @@
 require_relative '../classes/genre'
 require_relative '../classes/item'
 
-describe Genre do
-  context '#initialize' do
-    it 'creates a new instance of the Genre class' do
-      genre = Genre.new('action')
-      expect(genre).to be_an_instance_of(Genre)
-    end
-  end
-
-  context '#add_item' do
-    it 'sets the genre of an Item class\' instance to the instance of the Genre class' do
-      genre = Genre.new('action')
-      item = Item.new('2012/08/10')
+RSpec.describe Genre do
+  describe '#add_item' do
+    it "sets the genre of an Item class' instance to the instance of the Genre class" do
+      genre = Genre.new('Action')
+      item = Item.new 
       genre.add_item(item)
+
       expect(item.genre).to eq(genre)
-      expect(genre.items).to include(item)
     end
   end
 end
+
